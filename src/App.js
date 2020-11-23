2c5f3f934a38f67fe049800589527349ba838174
kail0n <kailontse@gmail.com>
2020-11-23 18:33:12 +0000

    add Discography component and linked to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import ArtistBio from './components/ArtistBio';
-// import Discography from './components/Discography';
+import Discography from './components/Discography';
 
 class App extends Component {
     render() {
         return (
             <main>
-                <h1>test</h1>
                 <ArtistBio />
-                {/* <Discography /> */}
+                <Discography />
             </main>
         )
     }
--- src/components/Discography.js
+++ src/components/Discography.js
@@ -1,3 +1,50 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// export default Discography;
+class Discography extends Component{
+        albums = {
+            record: [
+            {
+                title: 'MY NAME IS MICHAEL HOLBROOK',
+                image: 'https://cdn1.umg3.net/78/files/2019/10/mika-my-name-is-michael-holbrook-min.png',
+                linkToAlbum: 'https://www.mikasounds.com/releases/my-name-is-michael-holbrook/'
+            },
+            {
+                title: 'NO PLACE IN HEAVEN',
+                image: 'https://cdn1.umg3.net/78/files/2015/05/Screen-shot-2015-07-16-at-11.05.20.png',
+                linkToAlbum: 'https://www.mikasounds.com/releases/no-place-in-heaven/'
+    
+            },
+            {
+                title: 'SONGBOOK VOL. 1',
+                image: 'https://cdn1.umg3.net/78/files/2014/03/Mika-Song-Book-Vol.-1.jpg',
+                linkToAlbum: 'https://www.mikasounds.com/releases/songbook-vol-1/'
+            },
+        ],
+    };
+
+
+    render(){
+        return(
+            <div>
+                {this.albums.record.map((record) => {
+                    return (
+                        <>
+                            <h1>{record.title}</h1>
+                            <img src={record.image} />
+                            <a href={record.linkToAlbum}>Go to {record.title}</a>
+                        </>
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default Discography;
+
+
+
+
+
+
+
