67c88fac13dace89030a5ef2469a793dd78c7dea
kail0n <kailontse@gmail.com>
2020-11-24 13:18:25 +0000

    set up test suite (no test written yet)

---
+++ src/test/setupTests.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// in setupTests
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import sinon from 'sinon';
+
+global.React = React;
+global.shallow = shallow;
+global.sinon = sinon; // optional, if using sinon library
