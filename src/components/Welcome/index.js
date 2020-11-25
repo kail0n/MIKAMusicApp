fdb5d8114ebecbc44959526d6f60739d12a28c0f
kail0n <kailontse@gmail.com>
2020-11-25 10:43:36 +0000

    add Welcome component and test suite

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -6,11 +6,24 @@ class Welcome extends Component {
         nameInput: "",
     };
 
+    handleInput = e => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState(prevState => ({
+            username: prevState.nameInput,
+            nameInput: ""
+        }));
+    }
+
     render() {
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Hello {this.state.username ? this.state.username : "folk " } , welcome to Mika's Fanclub page</label>
-                <input type='text' name="nameInput" id='nameInput' placeholder='Enter your name!' />
+                <input type='text' name="nameInput" id='nameInput' placeholder='Enter your name!'  value={this.state.nameInput} onChange={this.handleInput} />
                 <input type='submit' value='Update!' />
             </form>
             )
